6440c016168b60f9d895eb4136497b330178132d
Ramon Ayco Jr <ramon.aycojr@infor.com>
2021-11-04 13:11:02 +0800

    remove unnecessary files; add lock file

--- updateemail.cmd
+++
@@ -1,8 +0,0 @@
-git filter-branch --commit-filter '
-        if [ "$GIT_AUTHOR_EMAIL" = "mail@ayco.io" ];
-        then
-                GIT_AUTHOR_EMAIL="ramon.aycojr@gmail.com";
-                git commit-tree "$@";
-        else
-                git commit-tree "$@";
-        fi' HEAD
